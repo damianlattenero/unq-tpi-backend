994d84d6bc931008da9b242cea733af31d2577be
Cristian Marchionne <cdmarchionne@gmail.com>
2016-09-07 22:18:39 -0300

    Se creo la clase Producto

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,35 @@
+package model;
+
+public class Product {
+    private String name;
+    private String description;
+
+    public Product() {
+    }
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " - " + this.description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
