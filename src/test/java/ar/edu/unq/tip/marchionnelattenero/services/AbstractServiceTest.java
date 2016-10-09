68f4339409a0fb5b353d946fd8581279d7745b35
Damian Lattenero <damianlattenero@gmail.com>
2016-10-09 00:22:24 -0300

    Abstract service test update

--- src/test/java/ar/edu/unq/tip/marchionnelattenero/services/AbstractServiceTest.java
+++ src/test/java/ar/edu/unq/tip/marchionnelattenero/services/AbstractServiceTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath*:config/BeanLocations.xml", "classpath*:database/DataSource.xml", "classpath*:database/Hibernate.xml"})
+@ContextConfiguration(locations = {"classpath*:/WEB-INF/webApplicationContext.xml", "classpath*:config/BeanLocations.xml", "classpath*:database/DataSource.xml", "classpath*:database/Hibernate.xml"})
 @Transactional(value = "persistence.transactionManager")
 public abstract class AbstractServiceTest {
 }
